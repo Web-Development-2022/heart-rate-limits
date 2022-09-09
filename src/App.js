18e006f8cfcd904da0c55086470e486a9cae3e37
Marion <marionroussel1@hotmail.fr>
2022-09-09 14:40:45 +0300

    heart rate limits week 2 exercise web dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [age, setAge] = useState(0)
+  const [hrupper, setUpper] = useState(0)
+  const [hrlower, setLower] = useState(0)
+
+  const calculate = () => {           //other way to syntax a function
+    const hrupper = (220 - age) * 0.85
+    setUpper(hrupper)
+
+    const hrlower = (220 -age) * 0.65
+    setLower(hrlower) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Heart rate limits calculator</h3>
+      <form>
+        <div>
+          <label>Age</label>
+          <input 
+            type = "number"
+            value = {age}
+            onChange = {e => setAge(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Heart rate limit: </label>
+          <output>{hrlower} - {hrupper}</output>
+        </div>
+      </form>
+      <button type="button" onClick={calculate}>Calculate</button>
     </div>
   );
 }
